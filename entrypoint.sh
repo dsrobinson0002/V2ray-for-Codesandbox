7d946ae41bfa78bb183d2cbc603dbe5b93e1e2cf
dsrobinson0002 <114290217+dsrobinson0002@users.noreply.github.com>
2023-05-01 05:43:17 +0330

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 # Web Site:https://www.hicairo.com
 # Telegram:https://t.me/HiaiFeng
 
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'7627fc7c-394b-4194-802d-db78922fc1d2'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
 URL=${HOSTNAME}-8080.csb.app
